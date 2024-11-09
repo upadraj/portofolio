84aa0c8db9bb650fa77a6ca4d5ab9ffa0a5ccb0a
Upadhayaya, Rajesh <upadraj@gatech.edu>
2024-11-09 08:34:17 -0700

    Production server gunicorn

--- personalWebsite/settings.py
+++ personalWebsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zpil8@29k#a=6gx2n!os3!@lp@*sy=w$rx(2h0wk2wy&5+pf4l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
@@ -112,6 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
